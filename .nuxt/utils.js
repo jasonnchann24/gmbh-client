1d46051928519c83e3ae99cef531fee2ddb22e2e
Jason  Chan <jasonnchann24@gmail.com>
2020-08-04 15:29:02 +0700

    build npm

--- .nuxt/utils.js
+++ .nuxt/utils.js
@@ -150,7 +150,7 @@ export async function setContext (app, context) {
       payload: context.payload,
       error: context.error,
       base: '/',
-      env: {"BASE_URL":"http://localhost:3000","API_URL":"https://api.jc-demo.com"}
+      env: {"BASE_URL":"https://jc-demo.com","API_URL":"https://api.jc-demo.com"}
     }
     // Only set once
     if (context.req) {
