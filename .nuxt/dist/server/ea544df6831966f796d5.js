f5bec4ecdadc03e8d3e2717a5dff483b88c2c680
Jason  Chan <jasonnchann24@gmail.com>
2020-08-12 15:23:15 +0700

    build

---
+++ .nuxt/dist/server/ea544df6831966f796d5.js
@@ -0,0 +1,1 @@
+exports.ids=[9],exports.modules={75:function(e,t,r){"use strict";r.r(t);var n={name:"PackagesPage"},c=r(1);var component=Object(c.a)(n,(function(){var e=this.$createElement;return(this._self._c||e)("div",[this._ssrNode("<h1>PACKAGES</h1>")])}),[],!1,(function(e){}),null,"33bac327");t.default=component.exports}};
