97bba0cf59b82a02100849560aaf18f239216ff1
JasonChan <jasonnchann24@gmail.com>
2020-08-21 18:46:07 +0700

    modal login fix disappearing

--- components/index/ModalLogin.vue
+++ components/index/ModalLogin.vue
@@ -1,227 +1,179 @@
 <template>
-  <div>
-    <div
-      id="modal-login"
-      class="modal fade"
-      tabindex="-1"
-      role="dialog"
-      data-backdrop="static"
-      data-keyboard="false"
-      aria-labelledby="modal-loginLabel"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content p-3" style="border-radius: 25px">
-          <div class="modal-header border-0">
-            <h5 id="modal-loginLabel" class="modal-title">
-              Welcome to CEO GmbH
-            </h5>
-            <button
-              id="closeModalBtn"
-              type="button"
-              class="close"
-              data-dismiss="modal"
-              aria-label="Close"
-              @click="clearForm"
-            >
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <div v-if="!register" class="container">
-              <div class="row">
-                <div class="col-12">
-                  <h3>Sign In</h3>
-                </div>
-                <div class="col-12 mt-4">
-                  <div class="form-group">
-                    <label for="login-email">Email address</label>
-                    <input
-                      id="login-email"
-                      v-model="loginForm.email"
-                      placeholder="Email"
-                      type="email"
-                      class="form-control"
-                      aria-describedby="emailHelp"
-                      required
-                      autocomplete="off"
-                    />
-                  </div>
-                  <div class="form-group">
-                    <label for="login-password">Password</label>
-                    <input
-                      id="login-password"
-                      v-model="loginForm.password"
-                      type="password"
-                      placeholder="Password"
-                      class="form-control"
-                      required
-                    />
-                  </div>
-                  <div class="row">
-                    <div class="col-12">
-                      <a
-                        :href="`${sanctum_url}/password/reset`"
-                        target="_blank"
-                      >
-                        Forget your password?
-                      </a>
-                    </div>
-                    <div class="col-12">
-                      <a href="javascript:void(0);" @click="changeModal">
-                        Need a new account? Register here.
-                      </a>
-                    </div>
-                    <div class="col-12 my-4 ">
-                      <button
-                        v-if="!loading"
-                        type="button"
-                        class="btn btn-block ceo-gmbh-btn mt-4 text-white"
-                        style="background-color: #4d89b9"
-                        @click="login"
-                      >
-                        Sign In
-                      </button>
-
-                      <button
-                        v-else
-                        class="btn btn-block ceo-gmbh-btn mt-4 text-white"
-                        style="background-color: #4d89b9"
-                        disabled
-                      >
-                        Logging in...
-                      </button>
-                    </div>
-                  </div>
-                </div>
+  <div
+    id="modal-login"
+    class="modal fade show"
+    tabindex="-1"
+    role="dialog"
+    data-backdrop="static"
+    data-keyboard="false"
+    aria-labelledby="modal-loginLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content p-3" style="border-radius: 25px">
+        <div class="modal-header border-0">
+          <h5 id="modal-loginLabel" class="modal-title">
+            Welcome to CEO GmbH
+          </h5>
+          <button
+            id="closeModalBtn"
+            type="button"
+            class="close"
+            data-dismiss="modal"
+            aria-label="Close"
+            @click="clearForm"
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div v-if="!register" class="container">
+            <div class="row">
+              <div class="col-12">
+                <h3>Sign In</h3>
               </div>
-            </div>
-
-            <div v-else-if="register" class="container">
-              <div class="row">
-                <div class="col-12">
-                  <h3 class="pt-2 pb-2 pr-2">Register</h3>
+              <div class="col-12 mt-4">
+                <div class="form-group">
+                  <label for="login-email">Email address</label>
+                  <input
+                    id="login-email"
+                    v-model="loginForm.email"
+                    placeholder="Email"
+                    type="email"
+                    class="form-control"
+                    aria-describedby="emailHelp"
+                    required
+                    autocomplete="off"
+                  />
                 </div>
-                <div class="col-12 mt-4">
-                  <div class="form-group">
-                    <label for="name-register">Full Name</label>
-                    <input
-                      id="name-register"
-                      v-model="form.name"
-                      type="text"
-                      class="form-control"
-                      required
-                      placeholder="Enter your full name"
-                    />
-                  </div>
-                  <div class="form-group">
-                    <label for="email-register">Email address</label>
-                    <input
-                      id="email-register"
-                      v-model="form.email"
-                      type="email"
-                      class="form-control"
-                      required
-                      placeholder="email@email.com"
-                    />
-                  </div>
-                  <div class="form-group">
-                    <label for="password-register">Password</label>
-                    <input
-                      id="password-register"
-                      v-model="form.password"
-                      type="password"
-                      class="form-control"
-                      required
-                    />
-                  </div>
-                  <div class="form-group">
-                    <label for="password-confirm-register"
-                      >Confirm Password</label
-                    >
-                    <input
-                      id="password-confirm-register"
-                      v-model="form.c_password"
-                      type="password"
-                      class="form-control"
-                      required
-                    />
-                  </div>
-                  <div class="row">
-                    <div class="col-12">
-                      <a
-                        href="javascript:void(0);"
-                        @click="
-                          changeModal()
-                          clearForm()
-                        "
-                      >
-                        Have an account? Login here.
-                      </a>
-                    </div>
-                    <div class="col-12">
-                      <a href="javascript:void(0);" @click="showResend">
-                        Not Receiving Verification Email?
-                      </a>
-                    </div>
-                    <div v-if="error" class="col-12 mt-4">
-                      <div class="alert alert-danger" role="alert">
-                        Error! Duplicate Email or wrong password confirmation!
-                      </div>
-                    </div>
-                    <div class="col-12 my-4">
-                      <button
-                        v-if="!loading"
-                        type="submit"
-                        class="btn btn-block ceo-gmbh-btn text-white"
-                        style="background-color: #4d89b9"
-                        @click="registerUser"
-                      >
-                        Register
-                      </button>
-                      <button
-                        v-else
-                        class="btn btn-block ceo-gmbh-btn text-white"
-                        style="background-color: #4d89b9"
-                        disabled
-                      >
-                        Loading ...
-                      </button>
-                    </div>
-                  </div>
+                <div class="form-group">
+                  <label for="login-password">Password</label>
+                  <input
+                    id="login-password"
+                    v-model="loginForm.password"
+                    type="password"
+                    placeholder="Password"
+                    class="form-control"
+                    required
+                  />
                 </div>
-              </div>
-            </div>
-            <div v-if="resend" class="container">
-              <div class="row">
-                <div class="col-12">
-                  <div class="form-group">
-                    <label for="email-resend">Resend Verification Email</label>
-                    <input
-                      id="email-resend"
-                      v-model="email"
-                      type="email"
-                      class="form-control"
-                      required
-                      placeholder="email@email.com"
-                    />
+                <div class="row">
+                  <div class="col-12">
+                    <a :href="`${sanctum_url}/password/reset`" target="_blank">
+                      Forget your password?
+                    </a>
                   </div>
-                  <div v-if="errorResend" class="col-12 mt-4">
-                    <div class="alert alert-danger" role="alert">
-                      <span v-for="(item, index) in errorResend" :key="index">
-                        {{ item }}
-                      </span>
-                    </div>
+                  <div class="col-12">
+                    <a href="javascript:void(0);" @click="changeModal">
+                      Need a new account? Register here.
+                    </a>
                   </div>
-                  <div class="col-12 my-4 mx-0 px-0">
+                  <div class="col-12 my-4 ">
                     <button
-                      v-if="!resendLoading"
+                      v-if="!loading"
+                      type="button"
+                      class="btn btn-block ceo-gmbh-btn mt-4 text-white"
+                      style="background-color: #4d89b9"
+                      @click="login"
+                    >
+                      Sign In
+                    </button>
+
+                    <button
+                      v-else
+                      class="btn btn-block ceo-gmbh-btn mt-4 text-white"
+                      style="background-color: #4d89b9"
+                      disabled
+                    >
+                      Logging in...
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div v-else-if="register" class="container">
+            <div class="row">
+              <div class="col-12">
+                <h3 class="pt-2 pb-2 pr-2">Register</h3>
+              </div>
+              <div class="col-12 mt-4">
+                <div class="form-group">
+                  <label for="name-register">Full Name</label>
+                  <input
+                    id="name-register"
+                    v-model="form.name"
+                    type="text"
+                    class="form-control"
+                    required
+                    placeholder="Enter your full name"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="email-register">Email address</label>
+                  <input
+                    id="email-register"
+                    v-model="form.email"
+                    type="email"
+                    class="form-control"
+                    required
+                    placeholder="email@email.com"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="password-register">Password</label>
+                  <input
+                    id="password-register"
+                    v-model="form.password"
+                    type="password"
+                    class="form-control"
+                    required
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="password-confirm-register"
+                    >Confirm Password</label
+                  >
+                  <input
+                    id="password-confirm-register"
+                    v-model="form.c_password"
+                    type="password"
+                    class="form-control"
+                    required
+                  />
+                </div>
+                <div class="row">
+                  <div class="col-12">
+                    <a
+                      href="javascript:void(0);"
+                      @click="
+                        changeModal()
+                        clearForm()
+                      "
+                    >
+                      Have an account? Login here.
+                    </a>
+                  </div>
+                  <div class="col-12">
+                    <a href="javascript:void(0);" @click="showResend">
+                      Not Receiving Verification Email?
+                    </a>
+                  </div>
+                  <div v-if="error" class="col-12 mt-4">
+                    <div class="alert alert-danger" role="alert">
+                      Error! Duplicate Email or wrong password confirmation!
+                    </div>
+                  </div>
+                  <div class="col-12 my-4">
+                    <button
+                      v-if="!loading"
                       type="submit"
                       class="btn btn-block ceo-gmbh-btn text-white"
                       style="background-color: #4d89b9"
-                      @click="resendEmail"
+                      @click="registerUser"
                     >
-                      Resend Email
+                      Register
                     </button>
                     <button
                       v-else
@@ -232,6 +184,49 @@
                       Loading ...
                     </button>
                   </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div v-if="resend" class="container">
+            <div class="row">
+              <div class="col-12">
+                <div class="form-group">
+                  <label for="email-resend">Resend Verification Email</label>
+                  <input
+                    id="email-resend"
+                    v-model="email"
+                    type="email"
+                    class="form-control"
+                    required
+                    placeholder="email@email.com"
+                  />
+                </div>
+                <div v-if="errorResend" class="col-12 mt-4">
+                  <div class="alert alert-danger" role="alert">
+                    <span v-for="(item, index) in errorResend" :key="index">
+                      {{ item }}
+                    </span>
+                  </div>
+                </div>
+                <div class="col-12 my-4 mx-0 px-0">
+                  <button
+                    v-if="!resendLoading"
+                    type="submit"
+                    class="btn btn-block ceo-gmbh-btn text-white"
+                    style="background-color: #4d89b9"
+                    @click="resendEmail"
+                  >
+                    Resend Email
+                  </button>
+                  <button
+                    v-else
+                    class="btn btn-block ceo-gmbh-btn text-white"
+                    style="background-color: #4d89b9"
+                    disabled
+                  >
+                    Loading ...
+                  </button>
                 </div>
               </div>
             </div>
