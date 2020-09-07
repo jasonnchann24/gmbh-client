fc436f2b47a4cae47929bf8014d6183d84b7e4a6
JasonChan <jasonnchann24@gmail.com>
2020-09-07 15:37:20 +0700

    initial i18n

--- .nuxt/axios.js
+++ .nuxt/axios.js
@@ -148,8 +148,8 @@ const setupProgress = (axios) => {
 export default (ctx, inject) => {
   // baseURL
   const baseURL = process.browser
-      ? 'https://api.jc-demo.com/api/'
-      : (process.env._AXIOS_BASE_URL_ || 'https://api.jc-demo.com/api/')
+      ? 'http://localhost:8000/api/'
+      : (process.env._AXIOS_BASE_URL_ || 'http://localhost:8000/api/')
 
   // Create fresh objects for all default header scopes
   // Axios creates only one which is shared across SSR requests!
